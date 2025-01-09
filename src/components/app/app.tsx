93029742ec67015f474136b1211a1ed22da3c5e9
ytka98 <rickda1ton@yandex.ru>
2025-01-09 19:47:38 +0300

    Сделал базовый роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,42 @@
-import { ConstructorPage } from '@pages';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  return (
+    <Router>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes>
+          <Route path="*" element={<NotFound404 />} />
+          <Route path="/" element={<ConstructorPage />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/profile">
+            <Route index element={<Profile />} />
+            <Route path="orders" element={<ProfileOrders />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
